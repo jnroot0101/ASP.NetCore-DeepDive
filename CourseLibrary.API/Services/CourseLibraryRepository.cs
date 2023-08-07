09881d336f64905583dbc878c11c5204fc52ab72
RedouaneJnah98 <redouanejnah2000@gmail.com>
2023-08-07 13:03:56 +0100

    Add DbContext

--- CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -1,9 +1,18 @@
+using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseLibrary.API.Services;
 
-public class CourseLibraryRepository: ICourseLibraryRepository
+public class CourseLibraryRepository : ICourseLibraryRepository
 {
+    private readonly CourseLibraryContext _context;
+
+    public CourseLibraryRepository(CourseLibraryContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
     public async Task<IEnumerable<Course>> GetCoursesAsync(Guid authorId)
     {
         throw new NotImplementedException();
@@ -11,12 +20,25 @@ public class CourseLibraryRepository: ICourseLibraryRepository
 
     public async Task<Course> GetCourseAsync(Guid authorId, Guid courseId)
     {
-        throw new NotImplementedException();
+        if (authorId == Guid.Empty) throw new ArgumentNullException(nameof(authorId));
+
+        if (courseId == Guid.Empty) throw new ArgumentNullException(nameof(courseId));
+
+#pragma warning disable CS8603
+        return await _context.Courses
+            .Where(c => c.AuthorId == authorId && c.Id == courseId).FirstOrDefaultAsync();
+#pragma warning restore CS8603
     }
 
     public void AddCourse(Guid authorId, Course course)
     {
-        throw new NotImplementedException();
+        if (authorId == Guid.Empty) throw new ArgumentNullException(nameof(authorId));
+
+        if (course == null) throw new ArgumentNullException(nameof(course));
+
+        // Always set the AuthorId to the passed-in authorId
+        course.AuthorId = authorId;
+        _context.Courses.Add(course);
     }
 
     public void UpdateCourse(Course course)
@@ -26,7 +48,7 @@ public class CourseLibraryRepository: ICourseLibraryRepository
 
     public void DeleteCourse(Course course)
     {
-        throw new NotImplementedException();
+        _context.Courses.Remove(course);
     }
 
     public async Task<IEnumerable<Author>> GetAuthorsAsync()
